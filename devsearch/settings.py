224af006822123fa43e38af62653c1cf5d72e16c
Raghavendra Khare <raghavxk@gmail.com>
2022-02-06 13:12:11 +0530

    disabled dbsqlite

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -101,12 +101,12 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
